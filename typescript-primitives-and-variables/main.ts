4509f34066a377510e4c34cd0f2b48ad0fef4e35
Kyle Spence <kyle.w.spence@gmail.com>
2025-02-03 22:07:35 +0000

    Completed typescript-primitives-and-variables.

---
+++ typescript-primitives-and-variables/main.ts
@@ -0,0 +1,19 @@
+const fullName: string = 'Kyle Spence';
+const isCool: boolean = true;
+const totalPets: number = 1;
+
+console.log('full name:', fullName);
+console.log('is cool?:', isCool);
+console.log('pets?', totalPets);
+
+console.log('typeof full name:', typeof fullName);
+console.log('typeof is cool:', typeof isCool);
+console.log('typeof total pets:', typeof totalPets);
+
+const empty: null = null;
+console.log('empty:', empty);
+console.log('typeof empty:', typeof empty);
+
+const nothing: undefined = undefined;
+console.log('nothing:', nothing);
+console.log('typeof nothing:', typeof nothing);
