7000da829670348ec18ac82a99cf6fb45960911b
Kyle Spence <kyle.w.spence@gmail.com>
2025-02-10 21:35:03 +0000

    typescript-loops-practice

--- typescript-loops-practice/add-suffix-to-all.ts
+++ typescript-loops-practice/add-suffix-to-all.ts
@@ -1,1 +1,8 @@
 /* exported addSuffixToAll */
+function addSuffixToAll(words: any[], suffix: any): any {
+  const newArray = [];
+  for (let i = 0; i < words.length; i++) {
+    newArray.push(words[i] + suffix);
+  }
+  return newArray;
+}
