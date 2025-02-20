605c4d02aa4b8702577bb9d9bfe13ee8d6b5922b
Kyle Spence <kyle.w.spence@gmail.com>
2025-02-20 22:20:56 +0000

    Completed typescript-forms exercise.

--- typescript-forms/form-values.js
+++ typescript-forms/form-values.js
@@ -1,12 +1,16 @@
-"use strict";
+'use strict';
 const $contactForm = document.querySelector('#contact-form');
 $contactForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const formElements = ;
-    const name;
-    const email;
-    const message;
-    const messageData = {
-        name, email, message,
-    };
+  event.preventDefault();
+  const $formElements = $contactForm.elements;
+  const name = $formElements.name.value;
+  const email = $formElements.email.value;
+  const message = $formElements.message.value;
+  const messageData = {
+    name,
+    email,
+    message,
+  };
+  console.log(messageData);
+  $contactForm.reset();
 });
