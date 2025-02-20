e987e216f4054868faff771b78976a60553a1ca5
Kyle Spence <kyle.w.spence@gmail.com>
2025-02-20 23:24:14 +0000

    string-manipulation-1

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,2 +1,7 @@
 'use strict';
 /* exported getWords */
+function getWords(input) {
+  if (input === '') return [];
+  return input.split(' ');
+}
+console.log(getWords('Kyle Spence'));
--- string-manipulation-1/src/get-words.ts
+++ string-manipulation-1/src/get-words.ts
@@ -1,1 +1,7 @@
 /* exported getWords */
+function getWords(input: string): string[] {
+  if (input === '') return [];
+  return input.split(' ');
+}
+
+console.log(getWords('Kyle Spence'));
