89eda5fcddc161c071d22f72848050453ca6bdb2
Kyle Spence <kyle.w.spence@gmail.com>
2025-03-04 22:33:45 +0000

    first few done

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,2 +1,13 @@
 'use strict';
 /* exported capitalizeWords */
+function capitalizeWords(string) {
+  const lowerCase = string.toLowerCase();
+  const result = lowerCase.split(' ');
+  for (let i = 0; i < result.length; i++) {
+    if (result[i].length > 0) {
+      result[i] = result[i][0].toUpperCase() + result[i].slice(1);
+    }
+  }
+  return result.join(' ');
+}
+console.log(capitalizeWords('javascript is cool'));
--- string-manipulation-2/src/ransom-case.ts
+++ string-manipulation-2/src/ransom-case.ts
@@ -1,1 +1,13 @@
 /* exported ransomCase */
+function ransomCase(string: string): any {
+  const lowerCase = string.toLowerCase();
+  const result = lowerCase.split('');
+  for (let i = 0; i < result.length; i++) {
+    if (i % 2 !== 0) {
+      result[i] = result[i].toUpperCase();
+    }
+  }
+  return result.join('');
+}
+
+console.log(ransomCase('kyle'));
