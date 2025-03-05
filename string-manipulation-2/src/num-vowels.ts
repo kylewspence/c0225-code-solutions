9cf423d35ead1e2cfb11d864d10bb55d0ce9c71d
Kyle Spence <kyle.w.spence@gmail.com>
2025-03-05 21:34:17 +0000

    vowels

--- string-manipulation-2/src/num-vowels.ts
+++ string-manipulation-2/src/num-vowels.ts
@@ -1,1 +1,10 @@
 /* exported numVowels */
+function numVowels(string: string): any {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  const lowerCase = string.toLowerCase();
+  const result = lowerCase.split('');
+  const vowelResult = result.filter((char) => vowels.includes(char));
+  return vowelResult.length;
+}
+
+console.log(numVowels('kyle is cool'));
