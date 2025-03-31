3946c0d93ac8521866a0b2963fa782399798c01f
Kyle Spence <kyle.w.spence@gmail.com>
2025-03-31 17:23:52 +0000

    Added reset on pause only and changed type for interval state.

--- react-stopwatch/my-app/src/StopWatch.tsx
+++ react-stopwatch/my-app/src/StopWatch.tsx
@@ -4,26 +4,32 @@ import { FaPause, FaPlay } from 'react-icons/fa';
 export function StopWatch() {
   const [elapsedTime, setElapsedTime] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [intervalId, setIntervalId] = useState<number | null>(null);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout>();
 
   function handleToggle() {
     if (!isPlaying) {
-      const id = window.setInterval(() => {
-        setElapsedTime((prev: number) => prev + 1);
+      const id = setInterval(() => {
+        setElapsedTime((prev) => prev + 1);
       }, 1000);
       setIntervalId(id);
       setIsPlaying(true);
     } else {
-      if (intervalId !== null) {
+      if (intervalId !== undefined) {
         clearInterval(intervalId);
-        setIntervalId(null);
+        setIntervalId(undefined);
       }
       setIsPlaying(false);
     }
   }
   return (
     <div className="stopwatch">
-      <div className="watch-face" onClick={() => setElapsedTime(0)}>
+      <div
+        className="watch-face"
+        onClick={() => {
+          if (!isPlaying) {
+            setElapsedTime(0);
+          }
+        }}>
         {elapsedTime}s
       </div>
       <button onClick={handleToggle}>
