1f8c9ca6e4f7496769162408fd212df7bde4a293
Kyle Spence <kyle.w.spence@gmail.com>
2025-03-31 18:39:49 +0000

    Setup

---
+++ react-search/my-app/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+import { UserSearch } from './Search';
+
+function App() {
+  return;
+  <>
+    <UserSearch />
+  </>;
+}
+
+export default App;
---
+++ react-search/my-app/src/Search.tsx
@@ -0,0 +1,10 @@
+export function UserSearch() {
+  return (
+    <div className="search-window">
+      <label>
+        Search:
+        <input type="text" name="searchbar" />
+      </label>
+    </div>
+  );
+}
