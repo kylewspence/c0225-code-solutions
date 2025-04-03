2931a997c89f50ced3d3a71523908840daa5551d
Kyle Spence <kyle.w.spence@gmail.com>
2025-04-03 21:01:54 +0000

    Completed catalog challenge.

---
+++ folders/react/react-catalog/my-app/src/pages/Dashboard.tsx
@@ -0,0 +1,79 @@
+import { useEffect, useState } from 'react';
+import { type Product, readCatalog } from '../lib/read';
+import { Link } from 'react-router-dom';
+import { toDollars } from '../lib/to-dollars';
+
+export function Dashboard() {
+  const [items, setItems] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<unknown>();
+
+  useEffect(() => {
+    async function loadItems() {
+      try {
+        const values = await readCatalog();
+        setItems(values);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    loadItems();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        Error! {error instanceof Error ? error.message : 'Unknown Error'}
+      </div>
+    );
+  }
+
+  return (
+    <div className="bg-white min-h-screen">
+      <div className="container">
+        <h1>Dashboard</h1>
+        <hr className="py-1" />
+        <div className="flex flex-wrap">
+          {items?.map((item) => (
+            <div
+              key={item.productId}
+              className="w-full md:w-1/2 lg:w-1/3 pr-4 pl-4">
+              <ItemCard item={item} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ItemCard({ item }: { item: Product }) {
+  return (
+    <div className="aspect-[4/5]">
+      <Link
+        to={`/details/${item.productId}`}
+        className="h-full flex flex-col overflow-hidden hover:shadow-g transition">
+        <div className="flex-grow overflow-hidden">
+          <img
+            src={item.imageUrl}
+            alt={item.name}
+            className="w-full h-full object-contain"
+          />
+        </div>
+        <div className="p-4">
+          <h5 className="text-lg font-bold mb-1 text-indigo-500">
+            {item.name}
+          </h5>
+          <p className="text-sm font-semibold">{toDollars(item.price)}</p>
+          <p className="text-sm text-gray-500 mb-1">{item.shortDescription}</p>
+        </div>
+      </Link>
+    </div>
+  );
+}
---
+++ folders/react/react-catalog/my-app/src/pages/Details.tsx
@@ -0,0 +1,79 @@
+import { useEffect, useState } from 'react';
+import { type Product, readProduct } from '../lib/read';
+import { useParams, useNavigate } from 'react-router-dom';
+import { toDollars } from '../lib/to-dollars';
+
+type Params = {
+  itemId: string;
+};
+
+export function Details() {
+  const { itemId } = useParams<Params>();
+  const [item, setItem] = useState<Product | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<unknown>();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function loadItem(itemId: number) {
+      try {
+        const item = await readProduct(itemId);
+        setItem(item);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    if (itemId) {
+      setIsLoading(true);
+      loadItem(+itemId);
+    } else {
+      setIsLoading(false);
+    }
+  }, [itemId]);
+
+  const handleAddToCart = () => {
+    alert('Product added to cart!');
+    navigate('/');
+  };
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error || !item) {
+    return (
+      <div>
+        Error Loading Item {itemId}:{' '}
+        {error instanceof Error ? error.message : 'Unknown Error'}
+      </div>
+    );
+  }
+  const { name, longDescription, imageUrl, price, shortDescription } = item;
+  return (
+    <div className="container text-gray-800 mt-8">
+      <div className="flex flex-col">
+        <div className="flex flex-wrap mb-4">
+          <div className="w-full sm:w-1/2 md:w-2/5 pt-2 px-4">
+            <img
+              src={imageUrl}
+              alt={name}
+              className="w-full h-80 object-contain"
+            />
+          </div>
+          <div className="w-full sm:w-1/2 md:w-3/5 px-4">
+            <h2 className="font-bold">{name}</h2>
+            <p className="font-bold">{toDollars(price)}</p>
+            <p className="whitespace-pre-wrap">{shortDescription}</p>
+          </div>
+        </div>
+        <div className="px-4">
+          <p className="whitespace-pre-wrap">{longDescription}</p>
+          <button
+            onClick={handleAddToCart}
+            className="mt-4 p-2 bg-blue-500 text-white">
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
