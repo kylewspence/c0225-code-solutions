4b5e511b91baa68e14fb7ad992e9fb504b8246e9
Kyle Spence <kyle.w.spence@gmail.com>
2025-04-14 16:10:36 +0000

    Completed exercise.

---
+++ folders/backend/express-routes/server.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('Hello, World!');
+  console.log('The date is', new Date());
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Here are your notes.');
+});
+
+app.get('/notes', (req, res) => {
+  res.send('Here are your notes.');
+});
+
+app.post('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.send(`You posted note with ID: ${noteId}`);
+});
+
+app.listen(8080, () => {
+  console.log('Express server listening on port 8080');
+});
